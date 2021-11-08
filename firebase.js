b72a123cc68c0d794a450f3452fabcfd07cd7496
Ateendra9 <58389068+Ateendra9@users.noreply.github.com>
2021-11-08 12:49:15 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCgJU6FPRjNmR4aTHv7-SXo1Gl2snTV0Ho",
+    authDomain: "not-medium.firebaseapp.com",
+    projectId: "not-medium",
+    storageBucket: "not-medium.appspot.com",
+    messagingSenderId: "1091821389079",
+    appId: "1:1091821389079:web:b69f54e7e9192c7c60a352"
+};
+firebase.initializeApp(firebaseConfig)
+let db = firebase.firestore();
